b427db7e16be75195825d73796bb9d2601c2a0af
yushoyamaguchi <ysh.824@outlook.jp>
2024-09-03 23:33:20 +0900

    v2

--- simple-controller/main.go
+++ simple-controller/main.go
@@ -26,6 +26,11 @@ var (
 		Version:  "v1",
 		Resource: "myresources",
 	}
+	configMapGVR = schema.GroupVersionResource{
+		Group:    "",
+		Version:  "v1",
+		Resource: "configmaps",
+	}
 )
 
 type Controller struct {
@@ -37,7 +42,8 @@ type Controller struct {
 func NewController(dynamicClient dynamic.Interface) *Controller {
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
-	informer := cache.NewSharedIndexInformer(
+	// MyResource Informer
+	myResourceInformer := cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 				return dynamicClient.Resource(myResourceGVR).Namespace(metav1.NamespaceAll).List(context.TODO(), options)
@@ -51,7 +57,7 @@ func NewController(dynamicClient dynamic.Interface) *Controller {
 		cache.Indexers{},
 	)
 
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	myResourceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			queue.Add(obj)
 		},
@@ -63,10 +69,31 @@ func NewController(dynamicClient dynamic.Interface) *Controller {
 		},
 	})
 
+	// ConfigMap Informer
+	configMapInformer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return dynamicClient.Resource(configMapGVR).Namespace(metav1.NamespaceAll).List(context.TODO(), options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return dynamicClient.Resource(configMapGVR).Namespace(metav1.NamespaceAll).Watch(context.TODO(), options)
+			},
+		},
+		&unstructured.Unstructured{},
+		0,
+		cache.Indexers{},
+	)
+
+	configMapInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		DeleteFunc: func(obj interface{}) {
+			queue.Add(obj)
+		},
+	})
+
 	return &Controller{
 		dynamicClient: dynamicClient,
 		queue:         queue,
-		informer:      informer,
+		informer:      myResourceInformer,
 	}
 }
 
@@ -75,8 +102,10 @@ func (c *Controller) Run(stopCh chan struct{}) {
 	defer c.queue.ShutDown()
 
 	go c.informer.Run(stopCh)
+	configMapInformer := c.NewConfigMapInformer()
+	go configMapInformer.Run(stopCh)
 
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced, configMapInformer.HasSynced) {
 		utilruntime.HandleError(fmt.Errorf("timed out waiting for caches to sync"))
 		return
 	}
@@ -95,10 +124,22 @@ func (c *Controller) Run(stopCh chan struct{}) {
 }
 
 func (c *Controller) processItem(obj interface{}) error {
-	resource := obj.(*unstructured.Unstructured)
-	fmt.Printf("Processing MyResource: %s/%s\n", resource.GetNamespace(), resource.GetName())
+	switch resource := obj.(type) {
+	case *unstructured.Unstructured:
+		if resource.GetKind() == "ConfigMap" {
+			return c.recreateConfigMap(resource)
+		} else {
+			return c.processMyResource(resource)
+		}
+	default:
+		return fmt.Errorf("unexpected type: %T", obj)
+	}
+}
 
-	unstructuredObj, err := c.dynamicClient.Resource(myResourceGVR).Namespace(resource.GetNamespace()).Get(context.TODO(), resource.GetName(), metav1.GetOptions{})
+func (c *Controller) processMyResource(obj *unstructured.Unstructured) error {
+	fmt.Printf("Processing MyResource: %s/%s\n", obj.GetNamespace(), obj.GetName())
+
+	unstructuredObj, err := c.dynamicClient.Resource(myResourceGVR).Namespace(obj.GetNamespace()).Get(context.TODO(), obj.GetName(), metav1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to get MyResource: %v", err)
 	}
@@ -110,7 +151,7 @@ func (c *Controller) processItem(obj interface{}) error {
 	configMap := &v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
-			Namespace: resource.GetNamespace(),
+			Namespace: obj.GetNamespace(),
 		},
 		Data: map[string]string{
 			configKey: configValue,
@@ -122,16 +163,47 @@ func (c *Controller) processItem(obj interface{}) error {
 		return fmt.Errorf("failed to convert ConfigMap to Unstructured: %v", err)
 	}
 
-	_, err = c.dynamicClient.Resource(schema.GroupVersionResource{
-		Group:    "",
-		Version:  "v1",
-		Resource: "configmaps",
-	}).Namespace(resource.GetNamespace()).Create(context.TODO(), &unstructured.Unstructured{Object: configMapUnstructured}, metav1.CreateOptions{})
+	_, err = c.dynamicClient.Resource(configMapGVR).Namespace(obj.GetNamespace()).Create(context.TODO(), &unstructured.Unstructured{Object: configMapUnstructured}, metav1.CreateOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to create configmap: %v", err)
 	}
 
 	return nil
+}
+
+func (c *Controller) recreateConfigMap(obj *unstructured.Unstructured) error {
+	name := obj.GetName()
+	namespace := obj.GetNamespace()
+
+	// MyResourceからConfigMapを再生成
+	myResourceList, err := c.dynamicClient.Resource(myResourceGVR).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list MyResources: %v", err)
+	}
+
+	for _, myResource := range myResourceList.Items {
+		if myResource.GetName() == name {
+			return c.processMyResource(&myResource)
+		}
+	}
+
+	return fmt.Errorf("no corresponding MyResource found for ConfigMap %s/%s", namespace, name)
+}
+
+func (c *Controller) NewConfigMapInformer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return c.dynamicClient.Resource(configMapGVR).Namespace(metav1.NamespaceAll).List(context.TODO(), options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return c.dynamicClient.Resource(configMapGVR).Namespace(metav1.NamespaceAll).Watch(context.TODO(), options)
+			},
+		},
+		&unstructured.Unstructured{},
+		0,
+		cache.Indexers{},
+	)
 }
 
 func main() {
